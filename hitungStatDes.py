60fd7aea6492f3b029f0a2a534d15403c5738ec2
b4its <exynsofam@gmail.com>
2025-05-02 05:35:13 +0800

    feat: menambahkan function untuk input data

--- hitungStatDes.py
+++ hitungStatDes.py
@@ -1,3 +1,19 @@
 import numpy as np
 import pandas as pd
 
+# Function untuk input data
+def inputData():
+    data = [] 
+    while True:
+        try:
+            nilai = float(input("Masukkan nilai data berupa angka: "))
+            data.append(nilai)
+        except ValueError:
+            print("Input tidak valid. hanya angka yang diperbolehkan.")
+            continue
+
+        lanjut = input("Apakah ingin menambahkan data lagi? (y/n): ").lower()
+        if lanjut != 'y':
+            break
+    return data 
+
