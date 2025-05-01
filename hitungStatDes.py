13295db9b36217905c6989155de26ea3de1bc01d
b4its <exynsofam@gmail.com>
2025-05-02 05:35:57 +0800

    feat: menambahkan function untuk pembuatana tabel distribusi frekuensi

--- hitungStatDes.py
+++ hitungStatDes.py
@@ -17,3 +17,27 @@ def inputData():
             break
     return data 
 
+# Function untuk membuat tabel distribusi frekuensi
+def distribusiFrekuensi(data, bins='sturges'):
+    if isinstance(bins, str):
+        if bins == 'sturges':
+            k = int(1 + 3.322 * np.log10(len(data)))
+        else:
+            raise ValueError("Hanya mendukung metode sturges.")
+    else:
+        k = bins
+
+    frek, bin_edges = np.histogram(data, bins=k)
+    intervals = [f"{round(bin_edges[i], 2)} - {round(bin_edges[i+1], 2)}" for i in range(len(bin_edges)-1)]
+    kelas = [i+1 for i in range(len(intervals))]
+    midpoints = [(bin_edges[i] + bin_edges[i+1]) / 2 for i in range(len(bin_edges)-1)]
+    
+    df = pd.DataFrame({
+        "Kelas Ke": kelas,
+        "Interval": intervals,
+        "Titik Tengah": midpoints,
+        "Frekuensi": frek
+    })
+    return df
+
+
